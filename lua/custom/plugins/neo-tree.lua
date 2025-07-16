3c506d26b725298aa46188fc84224942e5f273bb
nppetrov <nikola.petrov@student.uva.nl>
2025-07-16 14:49:43 +0200

    use kickstart neo-tree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,24 +1,30 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v3.x',
+  version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
+  lazy = false,
   keys = {
-    { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  config = function()
-    vim.g.neo_tree_remove_legacy_commands = 1
-    require('neo-tree').setup {
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
       window = {
-        position = 'left',
-        width = 30,
+        mappings = {
+          ['\\'] = 'close_window',
+        },
       },
-      filesystem = {
-        hijack_netrw_behavior = 'open_current',
-      },
-    }
-  end,
+    },
+  },
 }
